5f9424900bd175883da291fd6ba8da8ad6b51b5d
Maaz87 <maazkhanf391@gmail.com>
2026-05-20 16:23:35 +0500

    created a simple class styles which include butoon styles

---
+++ smart_counter_app/lib/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class buttonStyles  {
+
+  static final ButtonStyle primaryRed = ElevatedButton.styleFrom(
+    backgroundColor: Colors.red,
+    foregroundColor: Colors.white,
+    padding: EdgeInsets.only(left: 10,right: 10)
+  );
+
+
+}
